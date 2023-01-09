ab5293a8a070b93a74d7c657d07edc754914f9c5
LuigiAble <41165417+LuigiAble@users.noreply.github.com>
2023-01-09 10:51:22 -0500

    Update generate.js

--- cypress/support/generate.js
+++ cypress/support/generate.js
@@ -1,5 +1,5 @@
 export const randomEmail = () => {
   const randomValue = new Date().getTime();
-  const newEmail = `cloudapp+user+${randomValue}@testing.challenge.net`;
+  const newEmail = `cloudapp+user+${randomValue}@testing.e2e.net`;
   return newEmail;
 };
